cffc4b16f0c6ee35be155cb95b443691cf285fde
wasin siemsuk <guideserm@gmail.com>
2026-02-25 23:52:06 +0700

    Revert "Merge pull request #1 from PSDP-Palap/NOHUMAN"
    This reverts commit 07622bb6a10ba169c67c67d8d01c217b7155cb8c, reversing
    changes made to d9ed02a7b9ea26360f093ef653bfecee74102e8f.

--- main.go
+++ main.go
@@ -1,18 +1,3 @@
-// @title PALAP Backend API
-// @version 1.0
-// @description A comprehensive API for managing services, admins, freelancers, and products
-// @termsOfService http://swagger.io/terms/
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-// @host localhost:8080
-// @basePath /
-// @schemes http https
-// @securityDefinitions.apikey Authorization
-// @in header
-// @name Authorization
 package main
 
 import (
--- routers/router.go
+++ routers/router.go
@@ -15,49 +15,11 @@ func InitializeRouter() *gin.Engine {
 	apiV1 := r.Group("/api/v1")
 	apiV1.Use()
 	{
-		// Service endpoints (existing)
 		apiV1.GET("/service", v1.GetServices)
 		apiV1.GET("/service/:id", v1.GetService)
 		apiV1.POST("/service", v1.CreateBook)
 		apiV1.PUT("/service/:id", v1.UpdateService)
 		apiV1.DELETE("/service/:id", v1.DeleteService)
-
-		// User endpoints
-		apiV1.POST("/payment", v1.Payment)
-		apiV1.GET("/get_orders", v1.GetOrders)
-		apiV1.GET("/get_order_details", v1.GetOrderDetails)
-
-		// Admin management endpoints (CRUD)
-		apiV1.GET("/manage_admin", v1.GetAdmins)
-		apiV1.GET("/manage_admin/:id", v1.GetAdmin)
-		apiV1.POST("/manage_admin", v1.CreateAdmin)
-		apiV1.PUT("/manage_admin/:id", v1.UpdateAdmin)
-		apiV1.DELETE("/manage_admin/:id", v1.DeleteAdmin)
-
-		// Freelancer management endpoints (CRUD)
-		apiV1.GET("/manage_freelance", v1.GetFreelancers)
-		apiV1.GET("/manage_freelance/:id", v1.GetFreelancer)
-		apiV1.POST("/manage_freelance", v1.CreateFreelancer)
-		apiV1.PUT("/manage_freelance/:id", v1.UpdateFreelancer)
-		apiV1.DELETE("/manage_freelance/:id", v1.DeleteFreelancer)
-
-		// Freelancer specific endpoints
-		apiV1.POST("/registration", v1.RegisterFreelancer)
-		apiV1.GET("/get_freelance_job", v1.GetFreelanceJobs)
-
-		// Product and Service management endpoints (CRUD)
-		apiV1.GET("/manage_product_and_service", v1.GetProductServices)
-		apiV1.GET("/manage_product_and_service/:id", v1.GetProductService)
-		apiV1.POST("/manage_product_and_service", v1.CreateProductService)
-		apiV1.PUT("/manage_product_and_service/:id", v1.UpdateProductService)
-		apiV1.DELETE("/manage_product_and_service/:id", v1.DeleteProductService)
-
-		// Shop location management endpoints (CRUD)
-		apiV1.GET("/manage_shop_location", v1.GetShopLocations)
-		apiV1.GET("/manage_shop_location/:id", v1.GetShopLocation)
-		apiV1.POST("/manage_shop_location", v1.CreateShopLocation)
-		apiV1.PUT("/manage_shop_location/:id", v1.UpdateShopLocation)
-		apiV1.DELETE("/manage_shop_location/:id", v1.DeleteShopLocation)
 	}
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
